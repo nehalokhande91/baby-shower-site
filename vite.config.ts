fcfc9c1dbf8462e7eb7b4d2ce6366aedf8e6537c
nehalokhande <nehalokhande@nehalokhande.lan>
2026-01-30 23:20:19 -0800

    Fix deploy step

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/baby-shower-site/', // This must match your repository name exactly
+  base: '/baby-shower-site/', 
   build: {
     outDir: 'dist',
+    emptyOutDir: true,
   }
 });
