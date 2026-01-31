64e36c39c5177b9111884437fc4dce480aa8abd8
nehalokhande <nehalokhande@nehalokhande.lan>
2026-01-30 23:15:17 -0800

    deploy files

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/baby-shower-site/', // Matches your GitHub repo name
+  base: '/baby-shower-site/', // This must match your repository name exactly
+  build: {
+    outDir: 'dist',
+  }
 });
